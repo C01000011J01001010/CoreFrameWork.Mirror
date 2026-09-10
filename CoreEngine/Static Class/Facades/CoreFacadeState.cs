70dcf82dc7c01f874832cd4df0874497c2d68370
YoonChangJoon <ycjun1209@gmail.com>
2026-09-10 11:00:39 +0900

    Project 상태 모니터링 추가

--- CoreEngine/Static Class/Facades/CoreFacadeState.cs
+++ CoreEngine/Static Class/Facades/CoreFacadeState.cs
@@ -6,6 +6,7 @@ namespace CoreEngine.Facades
 {
     public static class CoreFacadeState
     {
+        public static bool ProjectInit => ProjectContext.Inst != null && ProjectContext.Inst.IsInit;
         public static bool SceneInit => SceneContext.Inst != null && SceneContext.Inst.IsInit;
     }
 }
