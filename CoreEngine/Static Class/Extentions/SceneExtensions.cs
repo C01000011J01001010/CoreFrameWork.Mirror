2724d32bf6226eb42bcc9519222b5deaffb95016
YoonChangJoon <ycjun1209@gmail.com>
2026-09-13 23:31:45 +0900

    GameObject의 Scene 이동을 돕는 확장메서드 추가

---
+++ CoreEngine/Static Class/Extentions/SceneExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CoreEngine.Extensions
+{
+    public static class SceneExtensions
+    {
+        public static void MoveScene(this GameObject obj, Scene targetScene)
+        {
+            SceneManager.MoveGameObjectToScene(obj, targetScene);
+        }
+    }
+}
