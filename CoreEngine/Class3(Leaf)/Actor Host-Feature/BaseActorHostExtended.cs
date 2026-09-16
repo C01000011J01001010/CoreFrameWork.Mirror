f226b81d25ff6fb02957ad4f3a829db7e0b7c735
YoonChangJoon <ycjun1209@gmail.com>
2026-09-16 17:36:48 +0900

    feature: BaseActorHostExtended를 추가하여 초기작업 최소화, OnSpawn, OnDespawn은 IPoolable을 구현하거나 OnEnable, OnDisable에서 호출

---
+++ CoreEngine/Class3(Leaf)/Actor Host-Feature/BaseActorHostExtended.cs
@@ -0,0 +1,30 @@
+using CoreEngine.Actor;
+using CoreEngine.Pool;
+using UnityEngine;
+
+namespace CoreEngine.Actor
+{
+    public abstract class BaseActorHostExtended : BaseActorHost, ISpawnable
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            RegisterFeatures();
+            FeatureHandler.Initialize_RegisteredFeatures();
+        }
+        protected abstract void RegisterFeatures();
+
+        public virtual void OnSpawn()
+        {
+            FeatureHandler.OnDespawn_InitializedFeatures();
+        }
+        public virtual void OnDespawn()
+        {
+            FeatureHandler.OnDespawn_InitializedFeatures();
+        }
+        protected virtual void OnDestroy()
+        {
+            FeatureHandler.Dispose_RegisteredFeatures();
+        }
+    }
+}
